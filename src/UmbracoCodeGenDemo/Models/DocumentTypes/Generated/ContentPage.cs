bd4c2848f21f7f8f96ef309709f422352e5db54e
danmalcolm <danmalc@gmail.com>
2015-01-25 13:33:46 +0000

    Add some templates using a template base class to create document

--- src/UmbracoCodeGenDemo/Models/DocumentTypes/Generated/ContentPage.cs
+++ src/UmbracoCodeGenDemo/Models/DocumentTypes/Generated/ContentPage.cs
@@ -3,8 +3,7 @@ namespace UmbracoCodeGenDemo.Models.DocumentTypes
     using System;
     using Umbraco.CodeGen.Annotations;
     
-    [DocumentType(Description="Basic editable content page", Icon=".sprTreeFolder", Thumbnail="folder.png", DefaultTemplate="ContentPage", AllowedTemplates=new String[] {
-            "ContentPage"}, Structure=new System.Type[] {
+    [DocumentType(Description="Basic editable content page", Icon=".sprTreeFolder", Thumbnail="folder.png", Structure=new System.Type[] {
             typeof(ContentPage)})]
     public partial class ContentPage : Page
     {
---
+++ src/UmbracoCodeGenDemo/Templates/DocumentTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+using UmbracoCodeGenDemo.Models.DocumentTypes;
+
+namespace UmbracoCodeGenDemo.Templates
+{
+    public abstract class DocumentTemplate<TDocument> : UmbracoViewPage<RenderModel>
+        where TDocument : Page
+    {
+        private TDocument document;
+
+        protected DocumentTemplate()
+        {
+        }
+
+        public TDocument Document
+        {
+            get
+            {
+                if (document == null)
+                {
+                    var ctor = typeof(TDocument).GetConstructors().First();
+                    document = ctor.Invoke(new object[] { Umbraco.AssignedContentItem }) as TDocument;
+                }
+                return document;
+            }
+        }
+    }
+}
